c62e2b6357de3e0e063ad1fd2c90b5aa11e4e004
Abdulazez <abzaek@hotmail.com>
2024-04-19 00:49:50 +0300

    deleted unnecessary folders

--- lib/create_team.dart
+++ lib/create_team.dart
@@ -2,7 +2,6 @@ import 'package:faq/util/colors.dart';
 import 'package:faq/util/custom_field.dart';
 import 'package:faq/util/my_button.dart';
 import 'package:flutter/material.dart';
-// import 'package:faq/util/app_bar.dart';
 
 void main() {
   runApp(const CreateTeam());
