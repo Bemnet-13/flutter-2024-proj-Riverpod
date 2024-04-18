a15f933908aa8783c7dcbe33cf0f7acb51bf0dc7
Sofonyas <sofstzz@gmail.com>
2024-04-18 06:18:14 +0300

    avatar list data

---
+++ lib/data/avatar_list_data.dart
@@ -0,0 +1,22 @@
+
+
+import '../models/avatar_model.dart';
+
+class AvatarListData{
+
+  final avatarList = [
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Paul Pogba', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+    AvatarModel(avatarClub: 'Newcastle', avatarName: 'Po', avatarPic: 'panda.jpeg'),
+
+  ];
+
+  
+}
