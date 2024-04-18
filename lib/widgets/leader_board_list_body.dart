fe01e25842702fd9f7e3f63addaa551efaa481c2
Sofonyas <sofstzz@gmail.com>
2024-04-18 06:25:32 +0300

    leader board and league list body

---
+++ lib/widgets/leader_board_list_body.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: prefer_const_constructors
+
+
+import 'package:flutter/material.dart';
+import '../data/leader_board_list_data.dart';
+
+
+class LeaderBoardListBody extends StatelessWidget{
+    const LeaderBoardListBody({super.key});
+
+    @override
+    Widget build(context){
+
+      final leaguelist = LeaderBoardListData().leaderboardlist;
+      return Padding(
+        padding:EdgeInsets.symmetric(horizontal:16),
+        child:Table( 
+        // border:TableBorder.all(),
+        children: [
+          TableRow(
+            decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.black, width: 2),)
+        ),
+          children: const [
+            TableCell(
+              child: Padding(
+                padding:EdgeInsets.all(16),
+                child:Center(child:Text(
+                'Rank',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32,),
+                
+              )),
+              )
+            ), 
+            Padding(
+                padding:EdgeInsets.all(16),
+                child: Center(child:Text(
+                'Player',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32,)),
+                
+              ),
+            ),
+            Padding(
+                padding:EdgeInsets.all(16),
+                child:Center(child:Text(
+                'Score',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32,),
+                
+              ),)
+            ),
+          ],
+        ),
+        ...List.generate(
+          leaguelist.length,
+          (index) => TableRow(
+            
+          decoration: BoxDecoration(
+            border: Border(
+            bottom: BorderSide(color: Colors.grey, width: 1,),),),
+          children: [
+
+              Padding(
+                padding:EdgeInsets.all(16),
+                child:Center(child:Text((index + 1).toString(), style: TextStyle(color:Colors.grey, fontSize: 24,),))),
+              Padding(
+                padding:EdgeInsets.all(8),
+                child:Center(child:Text(leaguelist[index].playername, style: TextStyle(color:Colors.grey, fontSize: 24),))),
+              Padding(
+                padding:EdgeInsets.all(8),
+                child:Center(child:Text(leaguelist[index].score.toString(), style: TextStyle(color:Colors.grey, fontSize: 24),))),
+
+            ],
+          )
+        ),
+        
+        ],
+      ));
+    }
+}
+
+
+
+
---
+++ lib/widgets/league_list_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../data/league_list_data.dart';
+import 'entries/league_list_entry.dart';
+
+class LeagueListBody extends StatelessWidget{
+  LeagueListBody({super.key});
+  final league = LeagueListData().leaguelistdata;
+  @override 
+  Widget build(context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+          child: Center(
+            child: Text(
+              'Join Leagues to compete with fellow contestants!',
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+
+        Expanded(child: 
+          
+          ListView.builder(
+            
+            itemCount: league.length,
+            itemBuilder: (context, index){
+              return LeagueListEntry(
+                index:index,
+                leaguelist: league,
+              );
+            }
+            )
+        ),
+      ],
+    );
+  }
+}
